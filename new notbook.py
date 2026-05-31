a17cdc8d8f2e64515593a7dd409c3a6d206e889c
Swejal <swejaldahare06@gmail.com>
2026-05-31 21:41:16 +0530

    Add files via upload

--- new notbook.py
+++ new notbook.py
@@ -1,3 +1,8 @@
-print("hello world")
-print("hello world")
-print("hello world")
+import pandas  as pd
+import matplotlib.pyplot as plt
+import seaborn  as sns
+from mpl_toolkits.axes_grid1 import host_axes
+
+df = pd.read_csv('ecommerce_sales_dataset.csv')
+print(df)
+
